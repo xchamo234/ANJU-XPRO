774f7d4ef7de5bd1296feb7efe3de86e741db170
BOT_RASHMIKA <janith60022006@gmail.com>
2025-03-08 18:22:09 +0530

    | 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗫ᴾᴿᴼ

--- session.js
+++ session.js
@@ -42,5 +42,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Your session id here",
 SUDO: process.env.SUDO || ["94717775628","94758775628"],
-PORT: process.env.PORT || "8000"
+PORT: process.env.PORT || "8000",
+FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
 };
